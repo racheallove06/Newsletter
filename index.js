1176106cfd360304d1ab25e76659a78c76c289b9
Racheal Muchiri <rachealloveo6@gmail.com>
2023-08-31 22:33:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function displayMessage() {
   let message = document.querySelector("success-container");
 }
-                              
+                                                 
 document
   .querySelector("submitButton")
   .addEventListener("click", displayMessage);
