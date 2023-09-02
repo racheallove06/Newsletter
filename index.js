1c4d0b0b0d896b16446b30ae8281656bf4a98815
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-02 21:39:01 +0300

    added the invalid email message

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function displayMessage(event) {
+  event.preventDefault(); // Prevent the form from submitting (page refresh)
 
+  let emailInput = document.querySelector("#email-input").value;
+  let errorElement = document.querySelector("#error-message");
+  let successContainer = document.querySelector("#success-container");
+  let userEmail = document.querySelector("#userEmail");
+  let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (emailInput === "") {
+    errorElement.textContent = "Please enter an email address.";
+  } else if (!emailPattern.test(emailInput)) {
+    errorElement.textContent = "Please enter a valid email address.";
+  } else {
+    errorElement.textContent = ""; // Clear any previous error message
+
+    // Display the success message
+    successContainer.style.display = "block";
+    userEmail.textContent = emailInput;
+  }
+}
+
+document
+  .querySelector("#submitButton")
+  .addEventListener("click", displayMessage);
