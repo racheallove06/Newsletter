563dec749284e4008cdabe4ff1cab224b720c121
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-02 22:06:10 +0300

    added the js interaction

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ function displayMessage(event) {
   let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
   if (emailInput === "") {
-    errorElement.textContent = "Please enter an email address.";
+    errorElement.textContent = "Valid email required.";
   } else if (!emailPattern.test(emailInput)) {
-    errorElement.textContent = "Please enter a valid email address.";
+    errorElement.textContent = "Valid email required.";
   } else {
     errorElement.textContent = ""; // Clear any previous error message
+
+    // Hide the entire form container
+    document.querySelector("#form-container").style.display = "none";
 
     // Display the success message
     successContainer.style.display = "block";
@@ -23,3 +26,15 @@ function displayMessage(event) {
 document
   .querySelector("#submitButton")
   .addEventListener("click", displayMessage);
+
+// Select the dismiss button by its ID
+const dismissButton = document.querySelector("#dismiss-button");
+
+// Add an event listener to the dismiss button
+dismissButton.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the default behavior of the button
+
+  // Show the initial content and hide the success message
+  document.querySelector("#form-container").style.display = "block";
+  document.querySelector("#success-container").style.display = "none";
+});
