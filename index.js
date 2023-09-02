e93a5b5ef0aa4c93e479fedb88e7fec4ac541e75
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-02 19:30:27 +0300

    added some javascript

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function displayMessage() {
-  let message = document.querySelector("success-container");
+function displayMessage(event) {
+  event.preventDefault(); // Prevent the form from submitting (page refresh)
+
+  let emailInput = document.querySelector("#email-input").value;
+  let successContainer = document.querySelector("#success-container");
+  let userEmail = document.querySelector("#userEmail");
+  let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (emailInput === "") {
+    alert("Please enter an email address.");
+  } else if (!emailPattern.test(emailInput)) {
+    alert("Please enter a valid email address.");
+  } else {
+    // Hide the form
+    document.querySelector("form").style.display = "none";
+
+    // Display the success message
+    successContainer.style.display = "block";
+    userEmail.textContent = emailInput;
+  }
 }
 
 document
-  .querySelector("submitButton")
+  .querySelector("#submitButton")
   .addEventListener("click", displayMessage);
